734ba73722a034b64f936f8ae7644a4d5015921f
nu11secur1ty <venvaropt@gmail.com>
2020-03-04 06:13:16 -0500

    CVE-2020-9442

---
+++ Undefined/CVE-2020-9442/Open_Vpn_vuln_test/OpenVPN/OpenVPN/MathLibrary.h
@@ -0,0 +1,34 @@
+// MathLibrary.h - Contains declarations of math functions
+#pragma once
+
+#ifdef MATHLIBRARY_EXPORTS
+#define MATHLIBRARY_API __declspec(dllexport)
+#else
+#define MATHLIBRARY_API __declspec(dllimport)
+#endif
+
+// The Fibonacci recurrence relation describes a sequence F
+// where F(n) is { n = 0, a
+//               { n = 1, b
+//               { n > 1, F(n-2) + F(n-1)
+// for some initial integral values a and b.
+// If the sequence is initialized F(0) = 1, F(1) = 1,
+// then this relation produces the well-known Fibonacci
+// sequence: 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+// Initialize a Fibonacci relation sequence
+// such that F(0) = a, F(1) = b.
+// This function must be called before any other function.
+extern "C" MATHLIBRARY_API void fibonacci_init(
+    const unsigned long long a, const unsigned long long b);
+
+// Produce the next value in the sequence.
+// Returns true on success and updates current value and index;
+// false on overflow, leaves current value and index unchanged.
+extern "C" MATHLIBRARY_API bool fibonacci_next();
+
+// Get the current value in the sequence.
+extern "C" MATHLIBRARY_API unsigned long long fibonacci_current();
+
+// Get the position of the current value in the sequence.
+extern "C" MATHLIBRARY_API unsigned fibonacci_index();
---
+++ Undefined/CVE-2020-9442/Open_Vpn_vuln_test/OpenVPNClient/OpenVPNClient/OpenVPNClient.cpp
@@ -0,0 +1,19 @@
+// MathClient.cpp : Client app for MathLibrary DLL.
+// #include "pch.h" Uncomment for Visual Studio 2017 and earlier
+#include <iostream>
+#include "MathLibrary.h"
+
+int main()
+{
+    // Initialize a Fibonacci relation sequence.
+    fibonacci_init(1, 1);
+    // Write out the sequence values until overflow.
+    do {
+        std::cout << fibonacci_index() << ": "
+            << fibonacci_current() << std::endl;
+    } while (fibonacci_next());
+    // Report count of values written before overflow.
+    std::cout << fibonacci_index() + 1 <<
+        " Fibonacci sequence values fit in an " <<
+        "unsigned 64-bit integer." << std::endl;
+}
